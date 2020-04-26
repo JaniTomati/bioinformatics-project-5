4afd344820a5d17853ef1758b801e34e9f23bc23
Jana Puschmann <jana.puschmann@post.au.dk>
2020-04-26 15:51:10 +0200

    Add comments. Need to figure out way to represent tree.

--- neighbor_joining.py
+++ neighbor_joining.py
@@ -32,7 +32,7 @@ def load_distance_matrix(file):
         return np.array(D), taxa
 
 
-def calculate_matrix_N(D):
+def calculate_matrix_N(D, S):
     """ Calculate N as defined by Saitou and Nei """
     N = np.zeros(D.shape)
     n, m = D.shape
@@ -40,7 +40,7 @@ def calculate_matrix_N(D):
     for i in range(n):
         for j in range(m):
             if i != j:
-                N[i, j] = D[i, j] - (1 / (n - 2) * sum(D[i, k] for k in range(n)) + 1 / (n - 2) * sum(D[j, k] for k in range(n)))
+                N[i, j] = D[i, j] - (1 / (len(S) - 2) * sum(D[i, k] for k in range(n)) + 1 / (len(S) - 2) * sum(D[j, k] for k in range(n)))
 
     return N
 
@@ -60,8 +60,8 @@ def find_neighbors(N):
     return pair
 
 
-def create_tree(T):
-    """ Create a tree in newick format using a list of taxa """
+def create_newick_tree(T):
+    """ Create a tree in newick format from the initial list of taxa """
     tree = "("
     for taxon in T:
         if taxon != T[-1]:
@@ -69,15 +69,32 @@ def create_tree(T):
         else:
             tree += taxon + ")"
 
-    return Phylo.read(io.StringIO(tree), "newick")
+    return tree
+    # return Phylo.read(io.StringIO(tree), "newick")
+
+
+def create_tree_graph(taxa):
+    """ Create a graph that represents our tree """
+    tree = nx.Graph()
+    tree.add_node("root") # insert a dummy root node which will be deleted later
+    tree.add_nodes_from(taxa)
+
+    for taxon in taxa:
+        tree.add_edge("root", taxon) # connect each taxon to the root
+
+    return tree
 
 
 def update_dissimilarity_matrix(D, i, j):
     """ Update the dissamilarity matrix """
+    shift = -1
+    if i > j:
+        shift = 0
+
     D_ = np.delete(D, i, 0)
     D_ = np.delete(D_, i, 1)
-    D_ = np.delete(D_, j - 1, 0) # -1 since we already deleted i from the matrix
-    D_ = np.delete(D_, j - 1, 1)
+    D_ = np.delete(D_, j + shift, 0) # shift j one to the left if i was already deleted
+    D_ = np.delete(D_, j + shift, 1)
 
     n, _ = D_.shape
     new = []
@@ -94,7 +111,7 @@ def update_dissimilarity_matrix(D, i, j):
     return D
 
 
-def update_taxa(S, i, j):
+def update_S(S, i, j):
     """ Update new list of taxa """
     taxon_i = S[i]
     taxon_j = S[j]
@@ -109,23 +126,50 @@ def update_taxa(S, i, j):
     return S
 
 
+def update_tree(tree, taxa, i, j, D):
+    """ Delete old nodes  and insert new nodes into the tree model """
+    print(tree.nodes())
+    tree.remove_node(taxa[i])
+    tree.remove_node(taxa[j])
+
+    tree.add_node(taxa[-1])
+    tree.add_node(taxa[i])
+    tree.add_node(taxa[j])
+
+    # weight_i = 1 / 2 * (D[i, j] + )
+    # weight_j =
+
+
+
 def neighbor_joining(D, taxa):
     """ Creates an unrooted binary tree such that similar species are grouped in the
         same sub tree and the tree distances correspond to the distance matrix if possible """
     S = copy.deepcopy(taxa)
-    # tree = create_tree(taxa)
+    tree = create_tree_graph(taxa) # create a newick format from the given taxa
     # Phylo.draw(tree)
     # Phylo.draw_ascii(tree)
 
     while len(S) > 3:
-        N = calculate_matrix_N(D)
+        # 1. a) Compute matrix N
+        N = calculate_matrix_N(D, S)
+
+        # 1. b) Select i, j such that it is a minimum entry in N
         i, j = find_neighbors(N)
 
-        S = update_taxa(S, i, j)
-        print(S)
+        # 2. Add node k to the tree
+
+        # 3. Add edges with weights to the tree
+
+        # 4. Update the dissimilarity matrix
         D = update_dissimilarity_matrix(D, i, j)
 
-    print(D)
+        # 5. Delete i and j from S and append the new taxon k
+        S = update_S(S, i, j)
+
+    # Add new node v
+
+
+    # Add edges to the tree
 
     return 0
 
